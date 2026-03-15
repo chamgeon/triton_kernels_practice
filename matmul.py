263ece21640054af16207b014129769aad7169bc
Mingeon Cha <mingeoncha@Mingeons-MacBook-Air.local>
2026-03-15 22:11:35 +0900

    matmul

---
+++ matmul.py
@@ -0,0 +1,163 @@
+import torch
+import triton
+import triton.language as tl
+
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
+
+def get_cuda_autotune_config():
+    return [
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=5,
+                      num_warps=2),
+        triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=5,
+                      num_warps=2),
+        # Good config for fp8 inputs.
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4)
+    ]
+
+@triton.autotune(
+    configs = get_cuda_autotune_config(),
+    key = ['M','N','K']
+)
+@triton.jit
+def matmul_kernel(
+    a_ptr, b_ptr, c_ptr,
+    M, N, K,
+    stride_am, stride_ak,
+    stride_bk, stride_bn,
+    stride_cm, stride_cn,
+    # META parameters
+    BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
+    GROUP_SIZE_M: tl.constexpr
+):
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M,BLOCK_SIZE_M)
+    num_pid_n = tl.cdiv(N,BLOCK_SIZE_N)
+    num_pid_in_group = GROUP_SIZE_M*num_pid_n
+    group_id = pid // num_pid_in_group
+    first_pid_m = group_id*GROUP_SIZE_M
+    group_size_m = min(GROUP_SIZE_M, num_pid_m - first_pid_m)
+    pid_m = first_pid_m + ((pid % num_pid_in_group) % group_size_m)
+    pid_n = (pid % num_pid_in_group) // group_size_m
+
+    tl.assume(pid_m >= 0)
+    tl.assume(pid_n >= 0)
+    tl.assume(stride_am > 0)
+    tl.assume(stride_ak > 0)
+    tl.assume(stride_bn > 0)
+    tl.assume(stride_bk > 0)
+    tl.assume(stride_cm > 0)
+    tl.assume(stride_cn > 0)
+
+    offs_am = (pid_m*BLOCK_SIZE_M + tl.arange(0,BLOCK_SIZE_M)) % M
+    offs_bn = (pid_n*BLOCK_SIZE_N + tl.arange(0,BLOCK_SIZE_N)) % N
+    offs_k = tl.arange(0,BLOCK_SIZE_K)
+    a_ptrs = a_ptr + (offs_am[:,None]*stride_am + offs_k[None,:]*stride_ak)
+    b_ptrs = b_ptr + (offs_k[:,None]*stride_bk + offs_bn[None,:]*stride_bn)
+
+    accumulator = tl.zeros((BLOCK_SIZE_M,BLOCK_SIZE_N), dtype = tl.float32)
+    for k in range(0,tl.cdiv(K,BLOCK_SIZE_K)):
+        a = tl.load(a_ptrs, mask = offs_k[None,:]<K-k*BLOCK_SIZE_K, other = 0.0)
+        b = tl.load(b_ptrs, mask = offs_k[:,None]<K-k*BLOCK_SIZE_K, other = 0.0)
+        accumulator = tl.dot(a,b,accumulator)
+        a_ptrs += BLOCK_SIZE_K * stride_ak
+        b_ptrs += BLOCK_SIZE_K * stride_bk
+    c = accumulator.to(tl.float16)
+
+    offs_cm = (pid_m*BLOCK_SIZE_M + tl.arange(0,BLOCK_SIZE_M)) % M
+    offs_cn = (pid_n*BLOCK_SIZE_N + tl.arange(0,BLOCK_SIZE_N)) % N
+    c_ptrs = c_ptr + offs_cm[:,None]*stride_cm + offs_cn[None,:]*stride_cn
+    c_mask = (offs_cm[:,None]<M) & (offs_cn[None,:]<N)
+    tl.store(c_ptrs, c, mask = c_mask)
+    
+
+
+def matmul(a, b):
+    assert a.shape[1] == b.shape[0], "Incompatible dimensions"
+    assert a.is_contiguous(), "Matrix A must be contiguous"
+    M,K = a.shape
+    K,N = b.shape
+    c = torch.empty((M,N), device = a.device, dtype = torch.float16)
+
+    grid = lambda META: (triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']), )
+    matmul_kernel[grid](
+        a, b, c,
+        M, N, K,
+        a.stride(0), a.stride(1),
+        b.stride(0), b.stride(1),
+        c.stride(0), c.stride(1)
+    )
+    return c
+
+
+def unit_test():
+    torch.manual_seed(0)
+    a = torch.rand((512,512), device = DEVICE, dtype = torch.float16) - 0.5
+    b = torch.rand((512,512), device = DEVICE, dtype = torch.float16) - 0.5
+    triton_output = matmul(a,b)
+    torch_output = torch.matmul(a,b)
+    if torch.allclose(triton_output, torch_output, atol=1e-2, rtol=0):
+        print("Triton and Torch match")
+    else:
+        print("Triton and Torch differ")
+
+
+configs = []
+configs.append(
+    triton.testing.Benchmark(
+        x_names=["M", "N", "K"],
+        x_vals=[128 * i for i in range(2, 33)],
+        line_arg="provider",
+        line_vals=["cublas", "triton"],
+        line_names=["cuBLAS", "Triton"],
+        styles=[("green", "-"), ("blue", "-")],
+        ylabel="TFLOPS",
+        plot_name="matmul-performance-fp16",
+        args={}
+    )
+)
+
+@triton.testing.perf_report(configs)
+def benchmark(M,N,K,provider):
+    a = torch.randn((M,K), dtype=torch.float16)
+    b = torch.randn((K,N), dtype=torch.float16)
+    quantiles = [0.5,0.2,0.8]
+    if provider == "cublas":
+        ms, min_ms, max_ms = triton.do_bench(lambda: torch.matmul(a,b), quantiles = quantiles)
+    if provider == "triton":
+        ms, min_ms, max_ms = triton.do_bench(lambda: matmul(a,b), quantiles = quantiles)
+    perf = lambda ms: 2*M*N*K*1e-12 / (ms*1e-3)
+    return perf(ms), perf(max_ms), perf(min_ms)
+
+if __name__ == "__main__":
+    unit_test()
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == "--benchmark":
+        benchmark.run(save_path='.', print_data=True)
+
